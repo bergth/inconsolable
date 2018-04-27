d0980d3091210b3c2edc73c464f764f268d78b31
DulcheE <eddy.dulche@live.fr>
2018-04-27 18:40:45 +0200

    fez
    fz

--- Jeux/test/test_commande/test_commande.ino
+++ Jeux/test/test_commande/test_commande.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+
+
+
+ // If using software SPI (the default case):
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
+
+Adafruit_SSD1306 tft(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+
+
+
+void update_screen() {
+  tft.display();
+  delay(2); //peut etre modifiee
+  tft.clearDisplay();
 }
 
-<<<<<<< HEAD
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+ //config pin
+  pinMode(3, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+
+  //init de l'ecran
+  tft.begin(SSD1306_SWITCHCAPVCC);
+
+}
 
 
 
@@ -77,9 +111,4 @@ void loop() {
   test_fillCircle();
   test_drawChar();
   test_drawString();
-=======
-void loop() {
-  // put your main code here, to run repeatedly:
->>>>>>> dc4aa70ef6e88f31b46ccdb61fd87b6e16dfa565
-
 }
